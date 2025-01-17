047ac231997e670fac56d8ad4f912d60a174947c
Sagar Joshi <coderjoshi15@gmail.com>
2025-01-17 15:34:10 +0530

    server1 commit

--- vite-project/src/Resetpass.jsx
+++ vite-project/src/Resetpass.jsx
@@ -11,7 +11,7 @@ function PassConfirm() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://gehuquiz-sagars-projects-0f20619e.vercel.app/reset-password/confirm${token}', {
+      const response = await fetch('https://gehuquiz-sagars-projects-0f20619e.vercel.app/reset-password/confirm/${token}', {
         method: 'POST',
         credentials: "include", // Include cookies
         headers: { 'Content-Type': 'application/json' },
