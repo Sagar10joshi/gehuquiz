169094c0ff133f4f19b68d95e0b87782cfbf28bd
Sagar Joshi <coderjoshi15@gmail.com>
2025-01-18 19:11:27 +0530

    new5 commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            'tsparticles': 'tsparticles',
-        },
-    },
-    base: '/', // Use '/' if deploying at the root of your domain or './' if deploying in a subdirectory
+  plugins: [react()],
+  base: './', // Use './' to generate relative paths for assets
 });
